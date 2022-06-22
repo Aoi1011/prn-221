779c1e0df001da955de29974cc05de11e854773a
Aoi <ht.so.ik0102@gmail.com>
2022-06-22 11:36:28 +0700

    Change

--- r-2022-06-22/single-responsibility-principle-demo/Program.cs
+++ r-2022-06-22/single-responsibility-principle-demo/Program.cs
@@ -4,7 +4,14 @@ using Newtonsoft.Json;
 
 class Program
 {
+    static List<Book> bookList;
     static void Main(string[] args)
+    {
+        // SingleResponsibilityDemo();
+        OpenClosedPrincipleDemo();
+    }
+
+    static void SingleResponsibilityDemo()
     {
         Console.WriteLine("List of Books");
         Console.WriteLine("-------------");
@@ -15,5 +22,27 @@ class Program
             Console.WriteLine($"{item.Title.PadRight(39, ' ')}" + $"{item.Author.PadRight(15, ' ')} {item.Price}");
         }
         Console.Read();
+    }
+
+    static void OpenClosedPrincipleDemo()
+    {
+        Console.WriteLine("Please, press 'yes' to read an extra file, ");
+        Console.WriteLine("or any other key for a single file");
+        var ans = Console.ReadLine();
+        bookList = (ans.ToLower() != "yes") ? Utilities.ReadData() : Utilities.ReadDataExtra();
+        PrintBooks(bookList);
+    }
+
+    static void PrintBooks(List<Book> books)
+    {
+        Console.WriteLine("List of Books");
+        Console.WriteLine("-------------");
+        var cadJson = File.ReadAllText("Data/BookStore_01.json");
+        var bookList = JsonConvert.DeserializeObject<Book[]>(cadJson);
+        foreach (var item in bookList)
+        {
+            Console.WriteLine($"{item.Title.PadRight(39, ' ')}" + $"{item.Author.PadRight(20, ' ')} {item.Price}");
+        }
+        Console.ReadLine();
     }
 }
---
+++ r-2022-06-22/single-responsibility-principle-demo/Utilities/Utilities.cs
@@ -0,0 +1,26 @@
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+internal class Utilities
+{
+    static string ReadFile(string filename)
+    {
+        return File.ReadAllText(filename);
+    }
+
+    internal static List<Book> ReadData()
+    {
+        var cadJSON = ReadFile("Data/BookStore_01.json");
+        var bookList = JsonConvert.DeserializeObject<List<Book>>(cadJSON);
+        return bookList;
+    }
+
+    internal static List<Book> ReadDataExtra()
+    {
+        List<Book> books = ReadData();
+        var cadJSON = ReadFile("Data/BookStore_02.json");
+        books.AddRange(JsonConvert.DeserializeObject<List<Book>>(cadJSON));
+        return books;
+    }
+}
