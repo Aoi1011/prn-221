cdb6067393ac012478855bac24bba25870885f8a
Aoi <ht.so.ik0102@gmail.com>
2022-06-23 11:05:28 +0700

    Remove unused file

--- r-2022-06-22/single-responsibility-principle-demo/Model/Book.cs
+++
@@ -1,6 +0,0 @@
-class Book : IBook
-{
-    public string Author { get; set; }
-    public double Price { get; set; }
-    public string Title { get; set; }
-}
--- r-2022-06-22/single-responsibility-principle-demo/Model/IBook.cs
+++
@@ -1,6 +0,0 @@
-interface IBook
-{
-    string Title { get; set; }
-    string Author { get; set; }
-    double Price { get; set; }
-}
